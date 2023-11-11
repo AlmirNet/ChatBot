6e1f20ba6776b9d5376eea2d48e0682d9014f7ad
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-11-11 20:17:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-import os
+import os 
+
+def processar_resposta(resposta, nome):
+    if resposta == '1':
+        print(f'{os.linesep}{nome} É bastante útil e paga muito bem. Futuramente, vai ser utilizado.{os.linesep}')
+    elif resposta == '2':
+        print(f'{os.linesep}{nome} É bastante útil e paga muito bem. Futuramente, vai ser utilizado.{os.linesep}')
+    elif resposta == '3':
+        print(f'{os.linesep}{nome} É bastante útil e paga muito bem. Futuramente, vai ser utilizado.{os.linesep}')
+    elif resposta == '4':
+        print(f'{os.linesep}{nome} É bastante útil e paga muito bem. Futuramente, vai ser utilizado.{os.linesep}')
+    else:
+        print('Digite apenas 1, 2, 3 ou 4')
+        
+
 def start():
     # Apresentar o chatbot
     print('Olá! Bem-vindo a AlmirNet.com')
@@ -6,9 +20,21 @@ def start():
     nome = input('Digite seu nome: ')
     # pedir o e-mail
     email = input('Digite seu e-mail: ')
-    # Oferecer o menu de opções
-    input(f'O que gostaria de saber hoje?{os.linesep}[1] - Vale a pena aprender Python?
-          {os.linesep}')
-    # Processar a resposta enviar
+    
+    while True:
+        # Oferecer o menu de opções
+        resposta = input(f'''O que gostaria de saber hoje?{os.linesep}
+        [1] - Vale a pena aprender Python?{os.linesep}
+        [2] - Quanto tempo leva para conseguir um emprego com Python?{os.linesep}
+        [3] - Quando vou saber que estou BOM o suficiente para conseguir um emprego?{os.linesep}
+        [4] - Onde me recomenda estudar Python para conseguir um emprego hoje?{os.linesep}
+        ''')
+
+        if resposta in ['1', '2', '3', '4']:
+            processar_resposta(resposta, nome)
+            break  # Sai do loop após processar a resposta válida
+        else:
+            print('Digite apenas 1, 2, 3 ou 4')
+
 if __name__ == '__main__':
     start()
